2acba8e7ed0be04c6fdd575970b926c274078505
Sean J Bennett <sjb@seanjbennett.com>
2023-03-06 23:50:14 -0500

    Adding top 3 news articles to TTS

--- WIP/main.py
+++ WIP/main.py
@@ -2,15 +2,18 @@
 # WIP - Desktop Assistant
 # Reference - https://www.youtube.com/watch?v=4k9CphTdnWE - ProgrammingWithHarry - Project1
 
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sprec #pip install speechRecognition
+import pyttsx3 # pip install pyttsx3
+import speech_recognition as sprec # pip install speechRecognition
 import datetime
 import calendar
-from num2words import num2words #pip install num2words
-import wikipedia #pip install wikipedia
+from num2words import num2words # pip install num2words
+import wikipedia # pip install wikipedia
 import os
 import smtplib
-import config
+from config import config
+from newsapi import NewsApiClient # pip install newsapi-python
+from urllib import request
+import json
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -21,9 +24,24 @@ def speak(textToSpeech):
     engine.runAndWait()
 
 def dailyIntro():
+    # Setting up TTS to say current date, month and ordinal calendar number
     month = datetime.datetime.now().month
     date = datetime.datetime.now().day
     textToSpeech = "Hello, the current date is " + calendar.day_name[date] + calendar.month_name[month] + num2words(date, ordinal=True)
+
+    # Setting up TTS to add on the current top news articles I am getting from this request
+    textToSpeech = textToSpeech + ". The top news articles in the US are: "
+    newsUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + config.api_key
+    
+    with request.urlopen(newsUrl) as response:
+        body = response.read()
+
+    newsItems = json.loads(body)
+
+    articles = newsItems['articles']
+    for title in range(0, 3):
+        titles = articles[title]
+        textToSpeech += "\n" + titles['title']
 
     engine.say(textToSpeech)
     engine.runAndWait()
